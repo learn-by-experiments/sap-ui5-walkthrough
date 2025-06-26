3987e14091c17f4c121c0a72e0ef848a0996f620
manoj9896 <manojkumar.nvsd@gmail.com>
2025-06-26 20:10:43 +0530

    create a view model in controller, add an input field to app
    using the JSONModel from ui5

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,8 +1,23 @@
 import MessageToast from "sap/m/MessageToast";
 import Controller from "sap/ui/core/mvc/Controller";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
+interface InputDataModel {
+  name: string;
+}
 export default class AppController extends Controller {
+  onInit(): void | undefined {
+    const data: InputDataModel = {
+      name: "Hello World",
+    };
+
+    const jsonModel = new JSONModel(data);
+    this.getView()?.setModel(jsonModel);
+  }
   onShowHello() {
-    MessageToast.show("Hello World");
+    const inputMessage: InputDataModel = (
+      this.getView()?.getModel() as JSONModel
+    ).getData();
+    MessageToast.show(inputMessage.name);
   }
 }
