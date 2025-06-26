0f89de6d73fd5dcff48bfb55d8395847a6d59cce
manoj9896 <manojkumar.nvsd@gmail.com>
2025-06-26 22:43:05 +0530

    - we move the texts of our UI to a separate resource file. - This way, they are all in a central place and can be easily translated into other languages. - This process of internationalization – in short i18n – is achieved in SAPUI5 by using a special resource model and the standard data binding syntax

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,6 +1,8 @@
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
 import MessageToast from "sap/m/MessageToast";
 import Controller from "sap/ui/core/mvc/Controller";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
 
 interface InputDataModel {
   name: string;
@@ -8,16 +10,28 @@ interface InputDataModel {
 export default class AppController extends Controller {
   onInit(): void | undefined {
     const data: InputDataModel = {
-      name: "Hello World",
+      name: "World",
     };
 
     const jsonModel = new JSONModel(data);
     this.getView()?.setModel(jsonModel);
+    // set i18n model on view
+    const i18nModel = new ResourceModel({
+      bundleName: "sap.ui5.walkthrough.i18n.i18n",
+    });
+
+    this.getView()?.setModel(i18nModel, "i18n");
   }
   onShowHello() {
-    const inputMessage: InputDataModel = (
-      this.getView()?.getModel() as JSONModel
-    ).getData();
-    MessageToast.show(inputMessage.name);
+    const inputMessage = (this.getView()?.getModel() as JSONModel).getProperty(
+      "/name"
+    );
+
+    const i18Bundle = (
+      this.getView()?.getModel("i18n") as ResourceModel
+    ).getResourceBundle() as ResourceBundle;
+    const msg = i18Bundle.getText("helloMsg", [inputMessage]);
+    MessageToast.show(msg as string);
+    console.log("check the msg : ", msg, i18Bundle);
   }
 }
