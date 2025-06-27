d0eb073714fb74c1c185556a2baae7dc8d21d74b
manoj9896 <manojkumar.nvsd@gmail.com>
2025-06-27 13:50:13 +0530

    - sap/ui/core/ComponentSupport module - can delete index.ts, because the descriptor now takes care of everything.

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -1,8 +1,5 @@
-import Control from "sap/ui/core/Control";
 import UIComponent from "sap/ui/core/UIComponent";
-import XMLView from "sap/ui/core/mvc/XMLView";
 import JSONModel from "sap/ui/model/json/JSONModel";
-import ResourceModel from "sap/ui/model/resource/ResourceModel";
 
 interface InputDataModel {
   name: string;
