205c61bafa3b25b68a981adb52b97739d85dd77e
manoj9896 <manojkumar.nvsd@gmail.com>
2025-06-30 13:06:29 +0530

    - removed the unwanted code from app.controller - referenced the HelloPanel.view in the App.view

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,20 +1,5 @@
-import ResourceBundle from "sap/base/i18n/ResourceBundle";
-import MessageToast from "sap/m/MessageToast";
 import Controller from "sap/ui/core/mvc/Controller";
-import JSONModel from "sap/ui/model/json/JSONModel";
-import ResourceModel from "sap/ui/model/resource/ResourceModel";
-
-export default class AppController extends Controller {
-  onShowHello() {
-    const inputMessage = (this.getView()?.getModel() as JSONModel).getProperty(
-      "/name"
-    );
-
-    const i18Bundle = (
-      this.getView()?.getModel("i18n") as ResourceModel
-    ).getResourceBundle() as ResourceBundle;
-    const msg = i18Bundle.getText("helloMsg", [inputMessage]);
-    MessageToast.show(msg as string);
-    console.log("check the msg : ", msg, i18Bundle);
-  }
-}
+/**
+ * @namespace sap.ui5.walkthrough.controller
+ */
+export default class AppController extends Controller {}
